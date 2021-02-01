65ee258bf8761c169003b1bd6886abc815ac3b2b
Michel Castillo <mcastillo@cswsolutions.com>
2021-02-01 07:51:30 -0600

    Comentando autorizacion

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const router = require('./router');
 const express = require('express');
 const UserModel = require('./schemas/user');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
 const http = require('http');
 const Errors = require('./errors');
 
 
-(async function () {
+(async function() {
     const app = express();
     config = await loadConfig();
     app.use(cors());
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
-    app.use(config.baseUrl, async (req, res, next) => {
+    app.use(config.baseUrl, async(req, res, next) => {
         if (await checkAccess(req)) {
             console.log('Authorized')
             return next();
-        }
-        else{
-            return res.status(Errors.Forbidden.code).send(Errors.Forbidden);
+        } else {
+            //return res.status(Errors.Forbidden.code).send(Errors.Forbidden);
+            return next();
         }
     });
 
@@ -68,18 +68,16 @@ async function checkAccess(request) {
 
     const headerValue = request.headers["authorization"];
     console.log(headerValue)
-    if (request.url === '/auth' || (request.url === '/users' && request.method === 'POST')){
+    if (request.url === '/auth' || (request.url === '/users' && request.method === 'POST')) {
         return true;
-    }
-    else if (headerValue!==undefined && headerValue.startsWith("Bearer ")) {
-        const user = await UserModel.findOne({_id: headerValue.split(" ")[1]}).exec();
+    } else if (headerValue !== undefined && headerValue.startsWith("Bearer ")) {
+        const user = await UserModel.findOne({ _id: headerValue.split(" ")[1] }).exec();
         //console.log(user);
-        if(user !== null && user.token !== null){
+        if (user !== null && user.token !== null) {
             return true;
         }
-        return false; 
-    }
-    else{
+        return false;
+    } else {
         return false;
     }
 }
@@ -87,45 +85,45 @@ async function checkAccess(request) {
 async function loadConfig() {
     var filePath = "config.json";
     try {
-        return new Promise((resolve,reject)=>{fs.readFile(filePath, { encoding: 'utf-8' }, async function (err, data) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(JSON.parse(data));
-            }
+        return new Promise((resolve, reject) => {
+            fs.readFile(filePath, { encoding: 'utf-8' }, async function(err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(JSON.parse(data));
+                }
 
-        });
-    })
-    }
-    catch (error) {
+            });
+        })
+    } catch (error) {
         return Promise.reject(new Error());
     }
 };
 
-async function getUserToken(jsonEmail){
-    return new Promise(async function(resolve, reject){
-        let key = await fs.readFileSync(config.key,'utf-8');
+async function getUserToken(jsonEmail) {
+    return new Promise(async function(resolve, reject) {
+        let key = await fs.readFileSync(config.key, 'utf-8');
         jwt.sign(jsonEmail, key, { algorithm: 'RS256', expiresIn: '24h' }, function(err, token) {
-            if(err){
+            if (err) {
                 reject(err);
             }
             //console.log(token);
             resolve(token);
         });
-    }) 
+    })
 }
 
-async function checkUserToken(token){
-    return new Promise(async function(resolve, reject){
-        let cert = await fs.readFileSync(config.cert,'utf-8');
-        jwt.verify(token, cert, {ignoreExpiration: true},function(err, token) {
-            if(err){
+async function checkUserToken(token) {
+    return new Promise(async function(resolve, reject) {
+        let cert = await fs.readFileSync(config.cert, 'utf-8');
+        jwt.verify(token, cert, { ignoreExpiration: true }, function(err, token) {
+            if (err) {
                 reject(err);
             }
             //console.log(token);
             resolve(token);
         });
-    }) 
+    })
 }
 
-module.exports = {checkAccess, getUserToken, checkUserToken}
+module.exports = { checkAccess, getUserToken, checkUserToken }
